caddb2076b087b303b326fd0b666d3c2d965d5d9
Oskar Ehnstrom <oskar@ehnstrom.net>
2015-10-01 18:38:06 +0300

    Add boilerplate php and css

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Hej TF</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400italic,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+  <h1>Hej TF</h1>
+
+  </body>
+</html>
