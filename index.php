b4961f4207bf23aa392fd087de388f4bd04e23f0
jkajan <jonatan.kajan@gmail.com>
2015-10-07 23:56:04 +0300

    Add webfonts

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   <title>Hej TF</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400italic,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
   <h1>Hej TF</h1>
+    <p>Hello wurld</p>
 
   </body>
 </html>
