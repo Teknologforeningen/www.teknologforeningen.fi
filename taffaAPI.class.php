e176f07d7881206a82209f903bbc3a431885bd40
VonLatvala <vonlatvala@cud5.wlan.kyla.fi>
2015-10-07 19:45:10 +0300

    Added taffaAPI

---
+++ taffaAPI.class.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of taffaAPI
+ *
+ * @author vonlatvala
+ */
+class taffaAPI
+{
+    public $langArr = Array('en' => 'en', 'fi' => 'fi', 'sv' => 'sv');
+    private $curLang = 'sv';
+    private $apiBaseURL = "http://api.teknolog.fi/taffa/";
+    
+    /**
+     * Returns a ready-to-use TAFFA API Object
+     * @param String $lang
+     * @param String $opt
+     */
+    public function __construct($lang = null)
+    {
+        $this->setLang($lang);
+    }
+    
+    /**
+     * Sets the language
+     * @param String $lang
+     * @return boolean true on success, false on failure
+     */
+    public function setLang($lang = null)
+    {
+        if(array_search($lang, $this->langArr, false))
+        {
+            $this->curLang = $lang;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public function getOpt($str)
+    {
+        #echo "Getting opt ".$str."\n";
+        switch ($str)
+        {
+            case 'daily':
+                return $this->getDaily();
+            default:
+                return false;
+        }
+    }
+    
+    public function getToday()
+    {
+        #echo "GETTING DAILY! \n";
+        /*
+        #echo "GETTING DAILY! \n";
+        if(date('H') >= 16 && date('N') === 5 || date('H') >= 15 && date('N') < 5)
+        {
+            #echo "IS AFTER 16 OR IS AFTER 15 ON FRIDAY!\n";
+            return $this->getTomorrow();
+        }
+        #echo "IS NOT AFTER 17!\n";
+        return $this->get('/html/0');*/
+        return $this->get('html/0/');
+    }
+    
+    public function getYesterday()
+    {
+        return $this->get('html/' . ((date('N')-1)>1?date('N'):7) . '/');
+    }
+    
+    public function getAtTheMoment()
+    {
+        $i18n = Array
+            (
+                'fi' => 'Toistaiseksi ravintola ei tarjoile ruokaa!',
+                'sv' => 'För tillfället serverar inte restaurangen mat!',
+                'en' => 'The restaurant is not serving food at the moment!'
+            );
+        #var_dump(Array('G' => (int)date('G'), 'i' => (int)date('i'), 'N' => (int)date('N')));
+        if (
+                ((int)date('G') < 10 || (int)date('G') === 10 && (int)date('i') < 30) // Pre 1030
+                || (int)date('G') >= 16 && (int)date('N') < 5 // Post 16 on mon-thu
+                || (int)date('G') >= 15 && (int)date('N') == 5 // Post 15 on fri
+                ||  (int)date('N') > 5 // sat-sun
+            )
+        {
+            #echo 'Returning i18n';
+            return $i18n[$this->curLang];
+        }
+        return $this->getToday();
+    }
+    
+    public function getTomorrow()
+    {
+        return $this->get('html/1');
+    }
+    
+    public function getDayN($n = null)
+    {
+        if(is_null($n))
+            $n = date('N');
+        return $this->get('html/'.$n);
+    }
+    
+    private function getHttpRequestCode($url)
+    {
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+    
+    private function get($url)
+    {
+        $i18n = Array
+            (
+                'fi' => 'Tietoa ei saatavilla!',
+                'sv' => 'Information ej tillgänglig!',
+                'en' => 'Information unavailable!'
+            );
+        $urlToGet = $this->apiBaseURL . $this->curLang . "/" . $url;
+        #echo "Getting ".$urlToGet;
+        $httpResponseCode = $this->getHttpRequestCode($urlToGet);
+        if(!in_array($httpResponseCode, Array(200, 301)))
+        {
+            #var_dump($this->getHttpRequestCode($urlToGet));
+            return $i18n[$this->curLang];
+        }
+        $res = file_get_contents($urlToGet);
+        #var_dump($res);
+        #echo "PERKELE";
+        return $res;
+    }
+    
+    public function __toString()
+    {
+        
+    }
+    
+    public function __destruct()
+    {
+        
+    }
+}
