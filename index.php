9367c5031e46f23bd3fd260dd014b00537c05c66
Walter <walter.berggren@gmail.com>
2015-10-12 19:02:26 +0200

    Added a menu bar as well as placeholder menu information

--- index.php
+++ index.php
@@ -1,14 +1,78 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
   <title>Hej TF</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/normalize.css">
+
   <link href='https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400italic,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body>
-  <h1>Hej TF</h1>
 
+  <script src="js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+  <nav class="top-bar" data-topbar role="navigation">
+    <ul class="title-area">
+      <li class="name">
+        <!-- The top bar will be misaligned if the title is removed: https://github.com/zurb/foundation/issues/918"-->
+        <h1><a href="#">Teknologföreningen</a></h1>
+      </li>
+      <!-- Adds the menu icon without any text-->
+      <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+    </ul>
+    <section class="top-bar-section">
+      <ul class="left">
+        <li><a href="#">Om Teknologföreningen</a></li>
+        <li><a href="#">Medlemsportal</a></li>
+        <li><a href="#">Arbiturienter</a></li>
+        <li><a href="#">Alumner</a></li>
+        <li><a href="#">Samarbete</a></li>
+      </ul>
+    </section>
+  </nav>
+  <div class="row">
+    <div class="small-6 small-centered column">
+      <img src="assets/tf_natside_logon-08.svg" alt="Teknologföreningens logga" style="max-height: 270px">
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 column">
+      <h3>Meny</h3>
+      <h6>Tisdag 5.5.2015</h6>
+      <p>Kalkon gordon bleu, gräddfilssås med vitlök S A
+        Morotspurésoppa L G T S A
+        Tonfisksallad VL G T S A
+        Grönsaksomelett L G T A
+        A la Carte: Grillkorvar i korg VL G A
+        Kom ihåg rabatten på à la carte med Elisa
+        Lompakko närbetalning</p>
+        <p>
+          Öppethållningstider:
+          Måndag - Torsdag: 10:30 - 16:00
+          Fredag: 10:30 - 15:00
+        </p>
+      </div>
+    </div>
+    <ul>
+      <li><a href="#"><img src="assets/tf_natside_logon-05.svg" style="height: 64px"><span><h3>Täffä</h3><p>Teknologrestaurang</p></span></a> </li>
+      <li><a href="#"><img src="assets/tf_natside_logon-06.svg" style="height: 64px"><span><h3>Täffä AB</h3><p>Beställningsrestaurang</p></span></a></li>
+      <li><a href="#"><img src="assets/tf_natside_logon-07.svg" style="height: 64px"><span><h3>Träffpunkt Aalto</h3></span></a></li>
+    </ul>
+    <footer>
+      <ul>
+        <li><a href="#">Suomeksi</a></li>
+        <li><a href="#">In English</a></li>
+      </ul>
+    </footer>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+    $(document).foundation();
+    </script>
   </body>
-</html>
+  </html>
