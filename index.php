ef5b6564c196cd3b45a7a64b3394893104136d67
Oskar Ehnstrom <oskar@ehnstrom.fi>
2016-01-01 23:13:03 +0200

    Use old website for about description

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ if ($lang != "sv" && $lang != "en" && $lang != "fi") {
     <section class="top-bar-section">
       <ul class="left">
         <?php if ($lang == "sv") { //All links in Swedish, only About and Cooperation in English and Finnish ?>
-          <li class="mobile-internal-links"><a href="https://medlem.teknologforeningen.fi/index.php/2-uncategorised/116-om-teknologfoereningen"><?php echo $translations[$lang]["about"]?></a></li>
+          <li class="mobile-internal-links"><a href="https://joomla.teknologforeningen.fi/sv/om-teknologfoereningen/presentation/presentation.html"><?php echo $translations[$lang]["about"]?></a></li>
           <li class="mobile-internal-links"><a href="https://medlem.teknologforeningen.fi"><?php echo $translations[$lang]["member"]?></a></li>
           <!-- <li class="mobile-internal-links"><a href="#"><?php echo $translations[$lang]["abi"]?></a></li> -->
           <!-- <li class="mobile-internal-links"><a href="#"><?php echo $translations[$lang]["alumni"]?></a></li> -->
@@ -156,7 +156,7 @@ if ($lang != "sv" && $lang != "en" && $lang != "fi") {
         <ul class="page-links-desktop">
 
           <?php if ($lang == "sv") { //All links in Swedish, only About and Cooperation in English and Finnish ?>
-            <li><a href="https://medlem.teknologforeningen.fi/index.php/2-uncategorised/116-om-teknologfoereningen"><?php echo $translations[$lang]["about"]?></a></li>
+            <li><a href="https://joomla.teknologforeningen.fi/sv/om-teknologfoereningen/presentation/presentation.html"><?php echo $translations[$lang]["about"]?></a></li>
             <li><a href="https://medlem.teknologforeningen.fi"><?php echo $translations[$lang]["member"]?></a></li>
             <!-- <li><a href="#"><?php echo $translations[$lang]["abi"]?></a></li> -->
             <!-- <li><a href="#"><?php echo $translations[$lang]["alumni"]?></a></li> -->
