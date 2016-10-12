a03a8ddf7d1e0288d6a1a3c7e5a485d38f4cefed
Walter Berggren <walter.berggren@gmail.com>
2016-10-12 20:57:15 +0300

    Add Google Analytics

--- index.php
+++ index.php
@@ -231,7 +231,7 @@ if ($lang != "sv" && $lang != "en" && $lang != "fi") {
               <tr>
                 <td><?php echo $translations[$lang]["fri"]?></td>
                 <!--<td>10:30 - 15:00</td>-->
-                <td><?php echo str_pad(TAFAPI_FRI_OPEN_H, 2, '0', STR_PAD_LEFT) .':'.str_pad(TAFAPI_FRI_OPEN_MIN, 2, '0', STR_PAD_LEFT). ' - ' 
+                <td><?php echo str_pad(TAFAPI_FRI_OPEN_H, 2, '0', STR_PAD_LEFT) .':'.str_pad(TAFAPI_FRI_OPEN_MIN, 2, '0', STR_PAD_LEFT). ' - '
                         .str_pad(TAFAPI_FRI_CLOSE_H, 2, '0', STR_PAD_LEFT).':'.str_pad(TAFAPI_FRI_CLOSE_MIN, 2, '0', STR_PAD_LEFT); ?></td>
               </tr>
             </table>
@@ -269,6 +269,15 @@ if ($lang != "sv" && $lang != "en" && $lang != "fi") {
   <script src="js/scripts.js"></script>
   <script>
   $(document).foundation();
+  </script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-19332456-6', 'auto');
+    ga('send', 'pageview');
   </script>
 </body>
 </html>
