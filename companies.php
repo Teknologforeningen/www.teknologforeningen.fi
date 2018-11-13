71eb317d9e37c51d615f0a9dc786f8accfed5ba9
Axel Latvala <axel@latvala.com>
2018-11-13 18:09:54 +0200

    removed roll-your-own as per tuba's request, but atleast the docker dev env PHP installation didn't support http_build_url(), so glued url parts together roll-your-own style.

--- companies.php
+++ companies.php
@@ -1,40 +1,29 @@
 <?php
 
 $suppLang = ['sv', 'fi', 'en'];
-// XXX: PLEASE SET THESE ACCORDINGLY BEFORE GOING INTO PRODUCTION
 $langArticleMap = [
-    'sv' => '614',
+    'sv' => '615',
     'fi' => '613',
-    'en' => '612'
+    'en' => '625'
 ];
 
 $lang = $suppLang[0];
 if (isset($_GET['lang']) && in_array((string)$_GET['lang'], $suppLang))
   $lang = $_GET['lang'];
 
-$forCompaniesObj = [
-    'proto' => 'https',
-    'domain' => 'about.teknologforeningen.fi',
-    'path' => '/index.php',
-    'getParam' => [
-        ['option', 'com_content'],
-        ['view', 'article'],
-        ['id', $langArticleMap[$lang]]
-    ]
+$getParam = [
+    'option' => 'com_content',
+    'view' => 'article',
+    'id' => $langArticleMap[$lang]
+];
+$urlObj = [
+    'scheme' => 'https',
+    'host' => 'about.teknologforeningen.fi',
+    'path' => '/index.php/'.$lang.'/',
+    'query' => http_build_query($getParam)
 ];
 
-$location = $forCompaniesObj['proto'].'://'.$forCompaniesObj['domain'].$forCompaniesObj['path'];
-$nGetParam = count($forCompaniesObj['getParam']);
-
-if($nGetParam > 0) {
-    $location .= '?';
-    for($getParamIdx = 0; $getParamIdx < $nGetParam; $getParamIdx++) {
-        $curParam = $forCompaniesObj['getParam'][$getParamIdx];
-        $location .= $curParam[0].'='.$curParam[1];
-        if($getParamIdx !== $nGetParam - 1)
-            $location .= '&';
-    }
-}
+$location = $urlObj['scheme'].'://'.$urlObj['host'].$urlObj['path'].'?'.$urlObj['query'];
 
 header('HTTP/1.1 301 Moved Permanently');
 header('Location: '.$location);
